f768667268193b87e5822b727beb6ddd5d5f599b
Devinne Leigh <devinneleigh@gmail.com>
2026-02-23 16:59:07 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/Web-Design-Game/",
+  base: "https://devinneleigh.github.io/Web-Design-Game/dist/",
   css: {
     preprocessorOptions: {
       scss: {
