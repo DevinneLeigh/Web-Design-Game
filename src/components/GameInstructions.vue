0961653a035d61659d909acfb6bbd2d5d75dab28
rstra9564 <rileystrasser@gmail.com>
2026-03-15 21:25:36 -0600

    Game Instructions - Update1

--- src/components/GameInstructions.vue
+++ src/components/GameInstructions.vue
@@ -1,5 +1,43 @@
 <template>
-<h2>Instructions</h2>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur a aspernatur consequatur iusto fugiat enim quod numquam aut aliquam molestiae asperiores quae quia, corrupti ut dignissimos, iure beatae. Exercitationem, nihil.</p>
+  <div class="instructions">
+    <h2>Instructions</h2>
 
+    <p>
+      Create a <strong>jumbotron hero section</strong> that highlights a main message on the page.
+    </p>
+
+    <p><strong>Follow these steps:</strong></p>
+
+    <ol>
+      <li>
+        In the HTML editor, create a container element with the class
+        <code>jumbotron</code>.
+      </li>
+
+      <li>
+        Inside this container, add an <code>h1</code> heading with the text
+        <strong>"Welcome to My Website"</strong>.
+      </li>
+
+      <li>
+        Below the heading, add a paragraph <code>(p)</code> with the text
+        <strong>"Learn web design by completing challenges."</strong>.
+      </li>
+    </ol>
+
+    <p><strong>Now style the section using CSS:</strong></p>
+
+    <ul>
+      <li>Select the <code>.jumbotron</code> class.</li>
+      <li>Add <code>padding</code> so there is space inside the container.</li>
+      <li>Give the section a <code>background-color</code> so it stands out.</li>
+      <li>Use <code>text-align</code> to center the content.</li>
+      <li>Increase the font size of the heading.</li>
+    </ul>
+
+    <p>
+      When finished, the page should display a large centered hero section with
+      a heading and supporting text.
+    </p>
+  </div>
 </template>
