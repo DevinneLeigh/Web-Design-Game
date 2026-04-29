1db6cc5255b382e0bc605a5d854735bef91b2bfa
rstra9564 <rileystrasser@gmail.com>
2026-04-29 12:48:54 -0600

    Changes to Tables Instructions

--- src/data/levels/htmlLevels.js
+++ src/data/levels/htmlLevels.js
@@ -323,36 +323,33 @@ export const htmlLevels = [
         <h2>Adding Tables</h2>
         <p>This activity will help you learn how to build a simple table using HTML and style it with basic CSS.</p>
 
-        <p>Tables are used to organize information into rows and columns, like a spreadsheet. You will create a table with a header row and some data.</p>
+        <p>You will create your own table from scratch using the correct HTML tags.</p>
 
-        <p>To start, use the &lt;table&gt; tag to create the table. Inside the table, use &lt;tr&gt; to create a row.</p>
+        <p>Start by adding a &lt;table&gt; element inside the body of your HTML file.</p>
 
-        <p>Inside each row, use &lt;th&gt; for header cells (these are usually bold) and &lt;td&gt; for regular data cells.</p>
+        <p>Inside the table, create a row using the &lt;tr&gt; tag. This will be your header row.</p>
 
-        <p>For example, a header row might include columns like Name, Age, and Favorite Language.</p>
+        <p>In that row, add three header cells using the &lt;th&gt; tag. Label them Name, Age, and Favorite Color.</p>
 
-        <p>After creating your table structure, you can use CSS to make it look better. You can add borders, spacing, and background colors.</p>
+        <p>Next, create a second row using another &lt;tr&gt; tag.</p>
 
-        <p>Use the "border" property to add lines around your table, and "padding" to create space inside each cell.</p>
+        <p>Inside this row, add three data cells using the &lt;td&gt; tag.</p>
 
-        <p>Try editing the example code by adding more rows or changing the text inside the table.</p>
+        <p>In the first box, type your name. In the second box, type your age. In the third box, type your favorite color.</p>
+
+        <p>After your table is complete, open your CSS file and style your table by adding borders, padding, and a background color for the header.</p>
+
+        <p>If you finish early, try adding another row for a friend or family member.</p>
     `,
     hint: "",
 
     starterCode: {
-      html: `<h1>My First Table</h1>
+      html: `
+
+  <h1>My First Table</h1>
 
   <table>
-    <tr>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Favorite Language</th>
-    </tr>
-    <tr>
-      <td>Example Name</td>
-      <td>00</td>
-      <td>Example Language</td>
-    </tr>
+    <!-- Add your table rows and cells here -->
   </table>
 `,
       css: `table {
