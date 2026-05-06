65cd96bc7a3a1e495481638d6055233cd81efc70
Devinne Leigh <devinneleigh@gmail.com>
2026-05-06 16:27:07 -0600

    Add project button

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,19 +1,45 @@
 <script setup>
-import { ref } from "vue"
-import { useRoute } from "vue-router"
-import { aboutGame } from "@/data/game"
-import LevelAccordion from "../components/LevelAccordion.vue"
-import Popup from '@/components/Popup.vue'
-const route = useRoute()
+  import { ref, computed } from "vue"
+  import { useRoute } from "vue-router"
+  import { useRouter } from "vue-router"
+  import { aboutGame } from "@/data/game"
+  import { useLevelStore } from '@/stores/levelStore'
+  import LevelAccordion from "../components/LevelAccordion.vue"
+  import Popup from '@/components/Popup.vue'
+  const route = useRoute()
+  const router = useRouter()
 
-const showLandingHelp = ref(false)
+  const showLandingHelp = ref(false)
+  const levelStore = useLevelStore()
 
-function openLandingHelp() {
-  showLandingHelp.value = true
-}
-function closeLandingHelp() {
-  showLandingHelp.value = false
-}
+  function openLandingHelp() {
+    showLandingHelp.value = true
+  }
+  function closeLandingHelp() {
+    showLandingHelp.value = false
+  }
+
+  const latestUnlockedProject = computed(() => {
+    const projects = levelStore.categories.playerProject
+
+    if (!projects) return null
+
+    const unlocked = projects.filter(p => p.unlocked)
+
+    if (unlocked.length === 0) return null
+
+    unlocked.sort((a, b) => {
+      const getNum = id => parseInt(id.replace(/\D/g, ""))
+      return getNum(b.id) - getNum(a.id)
+    })
+
+    return unlocked[0] 
+  })
+
+  function goToLatestProject() {
+    if (!latestUnlockedProject.value) return
+    router.push(`/level/${latestUnlockedProject.value.id}`)
+  }
 </script>
 
 <template>
@@ -35,6 +61,12 @@ function closeLandingHelp() {
     <div class="list-wrapper">
       <LevelAccordion />
     </div>
-
+    <button
+      v-if="latestUnlockedProject"
+      class="project-button"
+      @click="goToLatestProject"
+    >
+      Project →
+    </button>
   </div>
 </template>
